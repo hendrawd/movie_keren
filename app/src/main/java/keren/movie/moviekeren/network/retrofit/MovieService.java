adee4bc5d67ef002fa371b5082938210b02ca1fe
hendrawd <hendrawd88@gmail.com>
2017-08-06 12:41:10 +0700

    Tambah category dan ganti grid secara dinamis berdasarkan kategori

--- app/src/main/java/keren/movie/moviekeren/network/retrofit/MovieService.java
+++ app/src/main/java/keren/movie/moviekeren/network/retrofit/MovieService.java
@@ -3,6 +3,7 @@ package keren.movie.moviekeren.network.retrofit;
 import keren.movie.moviekeren.network.model.Movie;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * @author hendrawd on 7/30/17
@@ -10,6 +11,6 @@ import retrofit2.http.GET;
 
 public interface MovieService {
 
-    @GET("movie/popular")
-    Call<Movie> getPopularMovies();
+    @GET("movie/{category}")
+    Call<Movie> getMovies(@Path("category") String category);
 }
